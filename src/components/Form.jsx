ca44c72791027a881998d0af8194247452ff1550
MZETO <dani.martinez@tutanota.com>
2022-07-09 19:48:32 +0200

    INITIAL COMMIT

---
+++ src/components/Form.jsx
@@ -0,0 +1,16 @@
+import React, { useState }from "react";
+
+const Form = (props) => {
+
+    const INITIAL_STATE = {
+        rol: '',
+        email: '',
+        age: 0,
+        newsletter: '',
+        birthdate: 0,
+        password: ''
+    }
+
+
+
+}
